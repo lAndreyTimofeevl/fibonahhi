924ee74a02a7aa20e3aad6054274522b6d13fd82
Тимофеев Андрей <timofeev.1.98@yandex.ru>
2021-03-11 15:08:02 +0300

    Version1.0

---
+++ fibonahhi/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace fibonahhi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtbxCode = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(13, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите число";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // txtbxCode
+            // 
+            this.txtbxCode.Location = new System.Drawing.Point(304, 39);
+            this.txtbxCode.Multiline = true;
+            this.txtbxCode.Name = "txtbxCode";
+            this.txtbxCode.Size = new System.Drawing.Size(306, 55);
+            this.txtbxCode.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(304, 100);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(306, 139);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(622, 254);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.txtbxCode);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Код Фибоначчи";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtbxCode;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ fibonahhi/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fibonahhi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Заполните поля ввода значений", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            BigInteger search = BigInteger.Parse(textBox1.Text);
+            int n = Convert.ToInt32(textBox1.Text);
+            List<BigInteger> f = new List<BigInteger>();
+            BigInteger[] code = new BigInteger[n];
+
+            f.Add(0);
+            f.Add(1);
+
+            listBox1.Items.Add(f[0]);
+            listBox1.Items.Add(f[1]);
+            int i = 1;
+            while(f[i] <= n)
+            {
+                f.Add(f[i - 1] + f[i]);
+                listBox1.Items.Add(f[i]);
+                i++;
+            }
+            listBox1.Items.Add(f[i - 2] + f[i-1]);
+            txtbxCode.Text = Convert.ToString(fibcode(f, search, n));
+            listBox2.Items.Add(NUM(f, txtbxCode.Text, n));
+        }
+        BigInteger fibcode(IList<BigInteger> f, BigInteger search, int n)
+        {
+            int temp = n;
+            string str = "";
+            for (int i = f.Count -1; i > 0; i--)
+            {
+                if (f[i] <= search)
+                {
+                    temp = i;
+                    search = search - f[i];
+                    str += "1";
+                    for (int j = temp - 1; j > 0; j--)
+                    {
+                        if (f[j] <= search)
+                        {
+                            str += "1";
+                            search = search - f[j];
+                            temp = j;
+                        }
+                        else str += "0";
+                    }
+                }
+            }
+            str += "1";
+            BigInteger code = BigInteger.Parse(str);
+            return code;
+        }
+        BigInteger NUM(IList<BigInteger> f, string code, int n)
+        {
+            BigInteger sum = 0;
+            char[] arr = code.ToCharArray();
+            Array.Reverse(arr);
+            code = String.Concat<char>(arr);
+            BigInteger x = BigInteger.Parse(code);
+
+            var digitArray = x.ToString().Select(digit => BigInteger.Parse(digit.ToString())).ToArray();
+
+            for (int j = 0; j < digitArray.Length; j++)
+            {
+                if (digitArray[j] == 1)
+                {
+                    sum += f[j];
+                }
+            }
+            return sum;
+
+        }
+    }
+}
