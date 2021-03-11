999634fa464f9b500c23008cad5b2421517057b3
Dmitry Matin <super.dmitry320@yandex.ru>
2021-03-11 20:35:40 +0300

    Обновил интерфейс формы

--- fibonahhi/Form1.Designer.cs
+++ fibonahhi/Form1.Designer.cs
@@ -36,13 +36,15 @@ namespace fibonahhi
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txtbxCode = new System.Windows.Forms.TextBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 10);
+            this.button1.Location = new System.Drawing.Point(500, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.Size = new System.Drawing.Size(98, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Вычислить";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,51 +54,69 @@ namespace fibonahhi
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(13, 39);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(15, 52);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(285, 199);
+            this.listBox1.Size = new System.Drawing.Size(325, 264);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(15, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.Size = new System.Drawing.Size(110, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Введите число";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(106, 10);
+            this.textBox1.Location = new System.Drawing.Point(131, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.Size = new System.Drawing.Size(209, 27);
             this.textBox1.TabIndex = 3;
             // 
             // txtbxCode
             // 
-            this.txtbxCode.Location = new System.Drawing.Point(304, 39);
+            this.txtbxCode.Location = new System.Drawing.Point(347, 52);
+            this.txtbxCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtbxCode.Multiline = true;
             this.txtbxCode.Name = "txtbxCode";
-            this.txtbxCode.Size = new System.Drawing.Size(306, 55);
+            this.txtbxCode.Size = new System.Drawing.Size(349, 72);
             this.txtbxCode.TabIndex = 4;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(304, 100);
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(347, 133);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(306, 139);
+            this.listBox2.Size = new System.Drawing.Size(349, 184);
             this.listBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "на основе строк",
+            "на основе массива"});
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox1.Location = new System.Drawing.Point(346, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 28);
+            this.comboBox1.TabIndex = 6;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(622, 254);
+            this.ClientSize = new System.Drawing.Size(711, 339);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.txtbxCode);
             this.Controls.Add(this.textBox1);
@@ -104,6 +124,7 @@ namespace fibonahhi
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Код Фибоначчи";
@@ -120,6 +141,7 @@ namespace fibonahhi
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtbxCode;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
