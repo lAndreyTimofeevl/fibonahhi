95e5c864c465064847b4de70d1775b2f55c444da
Dmitry Matin <super.dmitry320@yandex.ru>
2021-03-11 20:27:53 +0300

    Добавил методы нахождения числа Фибоначчи, получения кода Фибоначчи из числа, а также получения числа из данного кода

---
+++ fibonahhi/FibCodesOnArray/FibonacciCodeGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using static FibonacciResearch.FibonacciGenerator;
+
+namespace FibonacciResearch
+{
+    public static class FibonacciCodeGenerator
+    {
+        public static string Encode(BigInteger number)
+        {
+            List<BigInteger> fibCache = new List<BigInteger>(32);
+            List<byte> code = new List<byte>(32);
+
+            BigInteger n = 0;
+            int i = 0;
+            while ((n = Fibonacci(i)) <= number)
+            {
+                fibCache.Add(n);
+                i++;
+            }
+
+            for (int j = fibCache.Count - 1; j >= 0; j--)
+            {
+                if (fibCache[j] <= number)
+                {
+                    number -= fibCache[j];
+                    code.Add(1);
+                }
+                else
+                    code.Add(0);
+            }
+
+            return string.Join(string.Empty, code);
+        }
+
+        public static BigInteger Decode(string code)
+        {
+            List<BigInteger> fibCache = new List<BigInteger>(32);
+            
+            byte[] codeArray = Array.ConvertAll<char, byte>(code.ToCharArray(), 
+                converter: (digit) => byte.Parse(digit.ToString()));
+
+            BigInteger value = new BigInteger(0);
+
+            int i = 0, j = codeArray.Length - 1;
+            while (i < codeArray.Length - 1)
+            {
+                fibCache.Add(Fibonacci(j));
+                if (codeArray[i] == 1)
+                    value += fibCache[i];
+
+                i++;
+                j--;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ fibonahhi/FibCodesOnArray/FibonacciGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace FibonacciResearch
+{
+    public static class FibonacciGenerator
+    {
+        public static BigInteger Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            BigInteger current = 0ul;
+            BigInteger next = 1ul;
+            BigInteger result = 0ul;
+
+            int count = 2;
+            while (count <= n)
+            {
+                result = checked(current + next);
+                current = next;
+                next = result;
+
+                count++;
+            }
+
+            return result;
+        }
+    }
+}
